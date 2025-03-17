2d74c035a7c92c737c4e8b5a645d96af7c78a1d6
kazmira28 <amorkazmira2828@gmail.com>
2025-03-17 17:08:16 +0800

    Create Activity 1.js

---
+++ Activity 1.js
@@ -0,0 +1,9 @@
+let name = "KAZMIRA AMOR SANTOS"; 
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "FEMALE"; 
+let twitterHandle = "@YourTwitterHandle";
+
+console.log("NAME:", KAZMIRA AMOR SANTOS);
+console.log("Ambition:",PROGRAMMER);
+console.log("Gender:", FEMALE);
+console.log("Twitter Handle:", twitterHandle);
