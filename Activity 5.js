124ede7b5c70e44012b095487b3839debbbd5589
kazmira28 <amorkazmira2828@gmail.com>
2025-03-17 17:14:28 +0800

    Create Activity 5.js

---
+++ Activity 5.js
@@ -0,0 +1,19 @@
+let favActorFirstName = "Tom";
+let favActorLastName = "Hanks"; 
+
+
+let fullName = favActorFirstName + " " + favActorLastName;
+
+
+let uppercase = fullName.toUpperCase();
+
+
+let message = "My favorite actor is " + uppercase + ".";
+
+message += " His best show is Silicon Valley."; // Replace with the actor's best show/movie
+
+console.log("First Name:", favActorFirstName);
+console.log("Last Name:", favActorLastName);
+console.log("Full Name:", fullName);
+console.log("Uppercase Name:", uppercase);
+console.log("Message:", message);
