efd542f3ba144bc3f1256ae9a133f514cd8fa9e0
kazmira28 <amorkazmira2828@gmail.com>
2025-03-17 17:13:12 +0800

    Create Activity 4.js

---
+++ Activity 4.js
@@ -0,0 +1,11 @@
+let firstFavNumb = 7; 
+let secondFavNumb = 3; 
+
+console.log("Greater than:", firstFavNumb > secondFavNumb);
+console.log("Less than:", firstFavNumb < secondFavNumb);
+console.log("Greater than or equal to:", firstFavNumb >= secondFavNumb);
+console.log("Less than or equal to:", firstFavNumb <= secondFavNumb);
+console.log("Strict equality (===):", firstFavNumb === secondFavNumb);
+console.log("Loose equality (==):", firstFavNumb == secondFavNumb);
+console.log("Strict inequality (!==):", firstFavNumb !== secondFavNumb);
+console.log("Loose inequality (!=):", firstFavNumb != secondFavNumb );
