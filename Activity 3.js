c519cebb8983023a14b52b339b0587a3e64ba3fb
kazmira28 <amorkazmira2828@gmail.com>
2025-03-17 17:11:46 +0800

    Create Activity 3.js

---
+++ Activity 3.js
@@ -0,0 +1,10 @@
+let isJsProgrammingLanguage = true; 
+let isJsHard = false; 
+let favNumb = 7; 
+let undefinedValue = favNumb + undefined;
+
+
+console.log("Is JavaScript a programming language?", isJsProgrammingLanguage);
+console.log("Is JavaScript hard?", isJsHard);
+console.log("Favorite Number:", favNumb);
+console.log("Result of adding favorite number with undefined:", undefinedValue);
